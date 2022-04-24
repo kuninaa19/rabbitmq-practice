5f3082c0f5d08f00cbbf7ee1dcd177c38af46279
kuninaa19 <minchan0527@naver.com>
2022-04-24 17:34:30 +0900

    Pub/Sub - exchange Type : direct (routing system)

---
+++ rabbitmq/src/main/kotlin/com/example/rabbitmq/routing/EmitLogDirect.kt
@@ -0,0 +1,45 @@
+package com.example.rabbitmq.routing
+
+import com.example.rabbitmq.RabbitmqApplication
+import com.rabbitmq.client.ConnectionFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmitLogDirect
+
+private fun getSeverity(args: Array<String>): String {
+    return args[0]
+}
+
+private fun getMessage(args: Array<String>): String {
+    return args[1]
+}
+
+fun main(args: Array<String>) {
+
+    val exchangeName: String = "direct_logs";
+
+    runApplication<RabbitmqApplication>(*args)
+
+    val factory = ConnectionFactory()
+    factory.username = "user"
+    factory.password = "password"
+    factory.host = "localhost"
+
+    factory.newConnection().use { connection ->
+        connection.createChannel().use { channel ->
+
+            /** @desc exchange type - direct
+             * a message goes to the queues whose binding key exactly matches the routing key of the message.
+             * */
+            channel.exchangeDeclare(exchangeName, "direct")
+
+            val severity: String = getSeverity(args)
+            val message: String = getMessage(args)
+
+            channel.basicPublish(exchangeName, severity, null, message.toByteArray(charset("UTF-8")))
+            println(" [x] Sent '$severity':'$message'")
+        }
+    }
+}
---
+++ rabbitmq/src/main/kotlin/com/example/rabbitmq/routing/ReceiveLogsDirect.kt
@@ -0,0 +1,47 @@
+package com.example.rabbitmq.routing
+
+import com.example.rabbitmq.RabbitmqApplication
+import com.rabbitmq.client.*
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.nio.charset.StandardCharsets
+
+
+@SpringBootApplication
+class ReceiveLogsDirect
+
+fun main(args: Array<String>) {
+
+    val exchangeName: String = "direct_logs";
+
+    runApplication<RabbitmqApplication>(*args)
+
+    val factory = ConnectionFactory()
+    factory.username = "user"
+    factory.password = "password"
+    factory.host = "localhost"
+
+    val connection: Connection = factory.newConnection()
+    val channel: Channel = connection.createChannel()
+
+    channel.exchangeDeclare(exchangeName, "direct")
+    val queueName: String = channel.queueDeclare().queue
+
+    if (args.isEmpty()) {
+        System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+        System.exit(1);
+    }
+
+    /** @Desc 특정한 값에 해당하는 데이터만 받도록 routingKey 설정 */
+    for (severity in args) {
+        channel.queueBind(queueName, exchangeName, severity)
+    }
+    println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery ->
+        val message = String(delivery.body, StandardCharsets.UTF_8)
+        println(" [x] Received '" + delivery.envelope.routingKey + "':'" + message + "'");
+    }
+    channel.basicConsume(queueName, true, deliverCallback) { consumerTag -> }
+
+}
